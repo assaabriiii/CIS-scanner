6f7e0f5cd4da9806a039d54e11c4567342518d5b
B <amirsbry1942@gmail.com>
2025-02-27 22:12:18 +0330

    added service check cdp

---
+++ IOS/IOS 15 17/parser_modules/ios17/services_parsers.py
@@ -0,0 +1,9 @@
+from ssh import ssh_send
+from report_temp.main_report import generate_report
+
+
+def compliance_check_cdp(connection, command, cis_check, level, global_report_output):
+    command_output = ssh_send(connection, command)
+    compliant = "cdp is not enabled" in command_output.lower()
+    current_configuration = command_output
+    global_report_output.append(generate_report(cis_check, level, compliant, current_configuration))
